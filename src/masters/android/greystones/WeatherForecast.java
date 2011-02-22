b200e0dce70f4a49b1de98448e6bb13b52c603ed
cjbooms <cjbooms@gmail.com>
2011-02-22 01:15:18 +0000

    Commit of all Unversioned Files

---
+++ src/masters/android/greystones/WeatherForecast.java
@@ -0,0 +1,23 @@
+package masters.android.greystones;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: conor
+ * Date: 20/02/11
+ * Time: 21:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class WeatherForecast extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weatherforecast);
+        WebView webview;
+        webview = (WebView) findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("http://m.yr.no/place/Ireland/Wicklow/Greystones/hour_by_hour.html");
+    }
+}
